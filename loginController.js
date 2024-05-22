6ae26718f5484ac805364a7257f699a9d958610f
RaizelArief <148044563+RaizelArief@users.noreply.github.com>
2024-05-22 09:15:24 +0700

    Update loginController.js

--- loginController.js
+++ loginController.js
@@ -1,23 +1,37 @@
-document.getElementById("loginForm") .addEventListener("submit",(event) => {
-event.preventDefault();
-const username = document.getElementById("username")
-const password = document.getElementById("password")
+document.getElementById("loginForm").addEventListener("submit", (event) => {
+  event.preventDefault();
+  const username = document.getElementById("username");
+  const password = document.getElementById("password");
 
-const loginButton = document.querySelector("#login-button")
-const storedUsername = localStorage.getItem ("username")
-const storedPassword =  localStorage.getItem ("password")
+  const loginButton = document.querySelector("#login-button");
 
-loginButton.onclick = () => {
-    console.log ("logged in")
-    if (username.value === storedUsername && password.value === storedPassword) {
-        alert("login Succes, selamat ${username.value}")
-        window.location.href = "index.html"
+  const registerButton = document.querySelector("#register-button")
+  loginButton.onclick = () => {
+    console.log("logged in");
+
+    const storedUsername = localStorage.getItem("username");
+    const storedPassword = localStorage.getItem("password");
+    let storedValid = false;
+    if (storedPassword == "" || storedUsername == "") {
+      localStorage.removeItem("username");
+      localStorage.removeItem("password");
+    } else {
+        storedValid = true;
     }
-    else {
-        alert("kamu gagal, silahkan coba lagi")
+
+
+    if (
+      username.value === storedUsername &&
+      password.value === storedPassword && storedValid
+    ) {
+      alert(`login Succes, welcome ${username.value}`);
+      window.location.href = "index.html";
+    } else {
+      alert("kamu gagal, silahkan coba lagi");
     }
+  };
 
-
-}
+  registerButton.onclick = () => {
+        window.location = "register.html"
+  }
 });
-    
